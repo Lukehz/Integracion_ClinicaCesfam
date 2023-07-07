8a632a68e7b42d9cd76be381c763d00e5753a444
lukehz <luc.hernandez@duocuc.cl>
2023-07-06 20:10:53 -0400

    ultimo commit

--- ClinicaCesfam/Controllers/HomeController.cs
+++ ClinicaCesfam/Controllers/HomeController.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using ClinicaCesfam.Models;
 
 namespace ClinicaCesfam.Controllers
 {
     public class HomeController : Controller
     {
+        private clinicaEntities db = new clinicaEntities();
         public ActionResult Index()
         {
+            //Recuento de pacientes
+            var pACIENTE = db.PACIENTE.Include(p => p.PERSONA);
+            ViewBag.pacientes = pACIENTE.Count();
+
+            //Recuento de pacientes
+            
+            ViewBag.medicamentos = db.MEDICAMENTO.Count();
+
+            //Recuento de pacientes
+            var mEDICO = db.MEDICO.Include(p => p.PERSONA);
+            ViewBag.medicos = mEDICO.Count();
+
+            var stockMedicamentos = db.Stock_medicamentos.ToList();
+
+            // Asignar los datos a ViewBag.StockMedicamentos
+            ViewBag.StockMedicamentos = stockMedicamentos;
+
             return View();
         }
 
